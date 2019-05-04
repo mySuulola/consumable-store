2dd0166805059dd0d8f50232eba6913623d69168
mySuulola <oluwaseyisuulola@gmail.com>
2019-05-04 12:49:33 +0100

    changed the mlab database and fixed some bugs

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,6 @@ const promisify = require('es6-promisify')
 const mail = require('../handlers/mail')
 
 
-
 exports.login = passport.authenticate("local", {
   failureRedirect: "/login",
   failureFlash: "Failed Login",
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,6 @@ exports.registerForm = async (req, res) => {
   res.render("register", { title: "Register" });
 };
 
-
 exports.validateRegister = async (req, res, next) => {
   req.sanitizeBody("name"); //from expressValidator middleware
   req.checkBody("name", "You must supply a name").notEmpty();
